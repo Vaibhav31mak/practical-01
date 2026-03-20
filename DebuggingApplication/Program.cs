5b7040c517ca8fd03da844742c10446973edb601
xyz <vaibhav.makwana@simformsolutions.com>
2026-03-20 13:43:58 +0530

    Completed the DebugginApplication

---
+++ DebuggingApplication/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Please Enter a Something: ");
+string userResponse = Console.ReadLine()!;
+if (Convert.ToBoolean(int.Parse(userResponse)))
+{
+    Console.WriteLine("The entered value is a integer");
+}
