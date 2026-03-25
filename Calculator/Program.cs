940e47f54d56b0519147fe8703ee4ff52117f1b1
xyz <vaibhav.makwana@simformsolutions.com>
2026-03-25 13:26:57 +0530

    Added readability and comments

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,15 +1,21 @@
 ﻿Console.WriteLine("This is a Calculator in which you will enter 2 numbers and the result will be obtained which would be the sum, subtraction, multiplication and division of the two numbers.");
+
 // Enter First Number
 Console.Write("Please Enter the First Number: ");
 int FirstNumber;
+
+// Error correction to enter the First Number again on entering wrong values.
 while (!int.TryParse(Console.ReadLine(), out FirstNumber))
 {
     Console.WriteLine("Not Entered a valid input. Please Enter a valid number as input.");
     Console.Write("Please Enter the First Number: ");
 }
-// Enter Second Number
+
+// Error correction to enter the First Number again on entering wrong values.
 Console.Write("Please Enter the Second Number: ");
 int SecondNumber;
+
+// Error correction and enter the numbers 
 while (!int.TryParse(Console.ReadLine(), out SecondNumber))
 {
     Console.WriteLine("Not Entered a valid input. Please Enter a valid number as input.");
